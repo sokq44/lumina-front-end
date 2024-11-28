dbc21a2bd4e7c37f1028c5ab09a04cdfd6f49643
sokq44 <sokq44@gmail.com>
2024-11-28 16:39:35 +0100

    chore: backend url provided via environmental variables

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
