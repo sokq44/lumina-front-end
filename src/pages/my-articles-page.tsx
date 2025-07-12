72ba306c18ee355fc35866bfe333353f09c89396
sokq44 <sokq44@gmail.com>
2025-07-12 22:17:06 +0200

    chore: made some minor (cosmetic) changes regarding suggested and my articles pages

--- src/pages/my-articles-page.tsx
+++ src/pages/my-articles-page.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { Input } from "@/components/ui/input";
-import { PenLine, Search } from "lucide-react";
+import { NotebookTabs, PenLine, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Container from "@/components/ui/container";
 import { Link, useNavigate } from "react-router-dom";
@@ -38,7 +38,13 @@ const MyArticlesPage = () => {
     <MediaQuery>
       <More>
         <Container className="w-screen h-screen flex flex-col">
-          <Container className="w-full h-32 px-12 flex items-end justify-center gap-x-2 mb-8">
+          <Container className="w-full flex items-center justify-center gap-x-3 mt-16">
+            <NotebookTabs size={44} className="mt-1 text-muted-foreground" />
+            <span className="text-6xl font-bold text-muted-foreground">
+              My Articles
+            </span>
+          </Container>
+          <Container className="w-full mt-16 px-12 flex items-end justify-center gap-x-2 mb-8">
             <Container className="w-full flex items-center justify-center">
               <Search className="text-muted-foreground border border-muted h-10 w-10 px-2 rounded-tl-md rounded-bl-md bg-muted" />
               <Input
@@ -62,6 +68,11 @@ const MyArticlesPage = () => {
             <LoadingScreen className="h-auto mt-32">
               Retrieving Articles...
             </LoadingScreen>
+          ) : !articles || articles.length <= 0 ? (
+            <Container className="flex items-center justify-center text-lg font-bold text-muted-foreground">
+              Sorry, but we couldn't find any articles written by you matching
+              provided phrase.
+            </Container>
           ) : (
             <Container className="grid md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8 mx-12">
               {articles?.map((article, index) => (
@@ -72,7 +83,7 @@ const MyArticlesPage = () => {
                 >
                   <ArticleCard
                     article={article}
-                    className="max-w-[20rem] mx-auto"
+                    className="shadow-md mx-auto"
                   />
                 </Link>
               ))}
@@ -82,22 +93,41 @@ const MyArticlesPage = () => {
       </More>
       <Less>
         <Container className="w-screen h-screen flex flex-col gap-y-4 px-4">
+          <Container className="w-full flex items-center justify-center gap-x-3 mb-8">
+            <NotebookTabs size={36} className="mt-1 text-muted-foreground" />
+            <span className="text-5xl font-bold text-muted-foreground">
+              My Articles
+            </span>
+          </Container>
           <Container className="w-full flex items-center justify-center mb-2">
             <Search className="text-muted-foreground border border-muted h-10 w-10 px-2 rounded-tl-md rounded-bl-md bg-muted" />
             <Input
+              ref={searchRef}
+              onChange={search}
               placeholder="Search..."
               className="border-l-0 border-muted rounded-none rounded-tr-md rounded-br-md"
             />
           </Container>
-          {articles?.map((article, index) => (
-            <Link
-              key={`article ${index}`}
-              to={`/article/${article.id}`}
-              state={{ article }}
-            >
-              <ArticleCard article={article} className="w-full shadow-md" />
-            </Link>
-          ))}
+          {isLoading ? (
+            <LoadingScreen className="h-auto mt-32">
+              Retrieving Articles...
+            </LoadingScreen>
+          ) : !articles || articles.length <= 0 ? (
+            <Container className="flex items-center justify-center text-center px-2 text-lg font-bold text-muted-foreground">
+              Sorry, but we couldn't find any articles written by you matching
+              provided phrase.
+            </Container>
+          ) : (
+            articles?.map((article, index) => (
+              <Link
+                key={`article ${index}`}
+                to={`/article/${article.id}`}
+                state={{ article }}
+              >
+                <ArticleCard article={article} className="w-full shadow-md" />
+              </Link>
+            ))
+          )}
         </Container>
       </Less>
     </MediaQuery>
--- src/pages/suggested-articles-page.tsx
+++ src/pages/suggested-articles-page.tsx
@@ -6,7 +6,7 @@ import Container from "@/components/ui/container";
 import ArticleCard from "@/components/ui/article-card";
 import LoadingScreen from "@/components/wraps/loading-screen";
 import { Less, MediaQuery, More } from "@/components/wraps/media-query";
-import { Search } from "lucide-react";
+import { Newspaper, Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 
 const SuggestedArticlesPage = () => {
@@ -36,7 +36,13 @@ const SuggestedArticlesPage = () => {
     <MediaQuery>
       <More>
         <Container className="w-screen h-screen">
-          <Container className="w-full h-32 px-12 flex items-end justify-center gap-x-2 mb-8">
+          <Container className="w-full flex items-center justify-center gap-x-3 mt-16">
+            <Newspaper size={44} className="mt-1 text-muted-foreground" />
+            <span className="text-6xl font-bold text-muted-foreground">
+              Suggested Articles
+            </span>
+          </Container>
+          <Container className="w-full mt-16 px-12 flex items-end justify-center gap-x-2 mb-8">
             <Container className="w-full flex items-center justify-center">
               <Search className="text-muted-foreground border border-muted h-10 w-10 px-2 rounded-tl-md rounded-bl-md bg-muted" />
               <Input
@@ -51,14 +57,14 @@ const SuggestedArticlesPage = () => {
             <LoadingScreen className="h-auto mt-32">
               Retrieving Articles...
             </LoadingScreen>
+          ) : !articles || articles.length <= 0 ? (
+            <Container className="flex items-center justify-center text-lg font-bold text-muted-foreground">
+              Sorry, but we couldn't find any articles matching provided phrase.
+            </Container>
           ) : (
             <Container className="grid md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8 mx-12">
               {articles?.map((article, index) => (
-                <Link
-                  key={`article ${index}`}
-                  to={`/article/${article.id}`}
-                  className=""
-                >
+                <Link key={`article ${index}`} to={`/article/${article.id}`}>
                   <ArticleCard
                     article={article}
                     className="shadow-md mx-auto"
@@ -71,22 +77,40 @@ const SuggestedArticlesPage = () => {
       </More>
       <Less>
         <Container className="w-screen h-screen flex flex-col gap-y-4 px-4">
+          <Container className="w-full flex items-center justify-center gap-x-3 mb-8">
+            <Newspaper size={36} className="mt-1 text-muted-foreground" />
+            <span className="text-5xl font-bold text-muted-foreground">
+              Suggested
+            </span>
+          </Container>
           <Container className="w-full flex items-center justify-center mb-2">
             <Search className="text-muted-foreground border border-muted h-10 w-10 px-2 rounded-tl-md rounded-bl-md bg-muted" />
             <Input
+              ref={searchRef}
+              onChange={search}
               placeholder="Search..."
               className="border-l-0 border-muted rounded-none rounded-tr-md rounded-br-md"
             />
           </Container>
-          {articles?.map((article, index) => (
-            <Link
-              key={`article ${index}`}
-              to={`/article/${article.id}`}
-              state={{ article }}
-            >
-              <ArticleCard article={article} className="w-full shadow-md" />
-            </Link>
-          ))}
+          {isLoading ? (
+            <LoadingScreen className="h-auto mt-32">
+              Retrieving Articles...
+            </LoadingScreen>
+          ) : !articles || articles.length <= 0 ? (
+            <Container className="flex items-center justify-center text-center text-lg font-bold text-muted-foreground">
+              Sorry, but we couldn't find any articles matching provided phrase.
+            </Container>
+          ) : (
+            articles?.map((article, index) => (
+              <Link
+                key={`article ${index}`}
+                to={`/article/${article.id}`}
+                state={{ article }}
+              >
+                <ArticleCard article={article} className="w-full shadow-md" />
+              </Link>
+            ))
+          )}
         </Container>
       </Less>
     </MediaQuery>
